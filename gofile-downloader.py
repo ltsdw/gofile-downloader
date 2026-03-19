3d2d2447118c712a4dcb7017734d821984a9f26c
ltsdw <44977415+ltsdw@users.noreply.github.com>
2026-03-18 22:48:27 -0300

    gofile-downloader: add new secret key

--- gofile-downloader.py
+++ gofile-downloader.py
@@ -84,7 +84,7 @@ def generate_website_token(user_agent: str, account_token: str) -> str:
     Generates the dynamic X-Website-Token required by GoFile API.
     """
     time_slot = int(time()) // 14400
-    raw = f"{user_agent}::en-US::{account_token}::{time_slot}::gf2026x"
+    raw = f"{user_agent}::en-US::{account_token}::{time_slot}::f4s58gs6"
     return sha256(raw.encode()).hexdigest()
 
 
